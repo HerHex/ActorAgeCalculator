5d66b8b900839c156ab803e8e7e1ca9013daa07b
HerHex <Cookieoxen@gmail.com>
2022-05-12 16:02:50 +0300

    Add project files.

---
+++ ActorAgeCalculator/MovieStar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ActorAgeCalculator
+{
+    public class MovieStar
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+    }
+}
---
+++ ActorAgeCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ActorAgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Мартин\Desktop\Interview Tasks\ActorAgeCalculator\input.txt");
+
+            List<MovieStar> items = JsonConvert.DeserializeObject<List<MovieStar>>(text);
+
+            foreach (var item in items)
+            {
+                var yearsOld = (DateTime.Today.Subtract(item.DateOfBirth)).TotalDays / 365.2425;
+                Console.WriteLine("{0} {1} {2} {3} {4} \n", item.Name,item.Surname + "\n",item.Nationality +"\n",item.Sex + "\n", $"Is {Math.Round(yearsOld)} years old !" );
+            }
+        }
+
+    }
+}
